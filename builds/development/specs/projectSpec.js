0871730edb4580fa20f1ade72bff2ebd11b03b66
@evalDev <WhatAGit@developingdan.com>
2016-07-23 20:26:49 +0100

    PASS - Calculator Mutiplies the last value with given value

--- builds/development/specs/projectSpec.js
+++ builds/development/specs/projectSpec.js
@@ -1,6 +1,9 @@
-/* globals describe, it, expect, add, multiply, divide, lastValue */
+/* globals describe, it, expect, add, multiply, divide, lastValue, beforeEach */
 "use strict";
 describe("Calculator", function(){
+  beforeEach(function() {
+      lastValue = 0;
+    });
   it("Mutiplies two numbers", function(){
     expect(multiply(2,4)).toBe(8);
     expect(multiply(62,2)).toBe(124);
@@ -19,5 +22,7 @@ describe("Calculator", function(){
   });
   it("Mutiplies the last calculated value with the new value", function(){
     expect(multiply(lastValue,4)).toBe(0);
+    expect(multiply(4,4)).toBe(16);
+    expect(multiply(lastValue,4)).toBe(64);
   });
 });
